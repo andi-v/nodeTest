c2fc9d935927fc7216d80ead26c353afdb5b259d
brainman07 <weaselwiz7@gmail.com>
2018-06-06 17:54:41 +0300

    node email

--- server.js
+++ server.js
@@ -1,28 +1,23 @@
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
-var formidable = require('formidable');
-var events = require('events');
+const http = require('http');
+const nodemailer = require('nodemailer');
 
 http.createServer(function (req, res) {
-    if (req.url == '/fileupload') {
-        var form = new formidable.IncomingForm();
-        form.parse(req, function(err, fields, files) {
-            var oldpath = files.filetoupload.path;
-            var newpath = 'C:/Users/andi/Documents/' + files.filetoupload.name;
-            fs.rename(oldpath, newpath, function (err) {
-                if (err) throw err;
-                res.write('File uploaded and moved');
-                res.end();
-            });
-        });
-    } else {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-        res.write('<input type="file" name="filetoupload"><br>');
-        res.write('<input type="submit">');
-        res.write('</form>');
-        return res.end();
-    }
+
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'weaselwiz7@gmail.com',
+            pass: 'snowFlake88'
+        }
+    });
+
+    var mailOptions = {
+        from: 'weaselwiz7@gmail.com',
+        to: 'weaselwiz7@gmail.com',
+        subject: 'Node.js email',
+        text: "blablabla"
+    };
+
+    transporter
 
 }).listen(8081);
